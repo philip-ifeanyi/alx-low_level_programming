d26a14e0ab9eb85bd526e5c0fd76ac9a6704efce
Philip Ifeanyi <philipifeanyi2000@gmail.com>
2022-10-13 23:18:09 -0700

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A function to print strings
+ * Return: return 0
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and tha piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	return (1);
+}

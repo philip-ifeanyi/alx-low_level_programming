c449a52fff2334b7167108e72d7f304b8b554bfc
Philip Ifeanyi <philipifeanyi2000@gmail.com>
2022-10-13 22:06:15 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - A function to print strings
+ * Return: return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

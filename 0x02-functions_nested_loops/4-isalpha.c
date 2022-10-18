e2d951caa329aac86056e8e2d9d558cd898f7439
Philip Ifeanyi <philipifeanyi2000@gmail.com>
2022-10-18 13:04:49 -0700

    Tasks isalph and sign

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - a function to check if a character is lowercase
+ * @c: the function accepts parameter c
+ *
+ * Return: Return 0 if false and 1 if true
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_sign - A function to print the sign of a number
+ * @n: An integer print_sign accepts for testing
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		putchar('+');
+		return (1);
+	else if (n == 0)
+		putchar(0);
+		return (0);
+	else
+		putchar('-');
+		return (-1);
+}

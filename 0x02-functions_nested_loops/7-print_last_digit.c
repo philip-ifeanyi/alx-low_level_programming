c5fad43bf5aea67d3b67bfd2c145072f2d32dd10
Philip Ifeanyi <philipifeanyi2000@gmail.com>
2022-10-19 11:47:20 -0700

    Task 7 done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,10 @@ int print_last_digit(int c)
 {
 	int last_digit = c % 10;
 
-	if (last_digit < 0) 
+	if (last_digit < 0)
 		last_digit *= -1;
 
-	putchar(last_digit + '0');
+	printf(last_digit + "0");
 
 	return (last_digit);
 }

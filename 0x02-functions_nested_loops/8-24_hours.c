0d80fa58905529917ab8e271e820baa1e786918b
Philip Ifeanyi <philipifeanyi2000@gmail.com>
2022-10-19 14:54:04 -0700

    task 8 and 9

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Function that prints every minute if the day, 24 hour clock
+ * h = hour, m = monites
+ * / 10 allows second digit to rotate
+ * for loop breaks before passing 24:00
+ *
+ * Return: 24 hour clock line by line
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - A function to print the times table until 9
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 1; j <= 9; j++)
+		{
+			d = i * j;
+			d /= 10;
+
+			if (d > 0)
+			{
+				_putchar(d + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+
+			_putchar(d + '0');
+
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}

fcc4444713b3f75789dcdf77edc0210f36245e6f
Philip Ifeanyi <philipifeanyi2000@gmail.com>
2022-10-24 13:21:23 -0700

    Task 2 done, strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - A function to count and return the length of a string
+ * @s: pointer to the valur of the string to be counted
+ *
+ * Return: ALways return the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != "\0")
+	{
+		count++;
+		ch++;
+	}
+
+	return (count);
+}

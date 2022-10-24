90c5698820e6d7acf274b9861fccb394d8a8eb40
Philip Ifeanyi <philipifeanyi2000@gmail.com>
2022-10-24 12:47:15 -0700

    Initialized folder, added readme, _putchar, main.c and started task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - a function to update the value of a pointer to 98
- * @*n: pointer to the address of the integer to be tested
+ * @n: pointer to the address of the integer to be tested
  */
 
 void reset_to_98(int *n)

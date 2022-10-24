62b0b46e60d868286fc9195046ea83cca373563b
Philip Ifeanyi <philipifeanyi2000@gmail.com>
2022-10-24 12:40:53 -0700

    Initialized folder, added readme, _putchar, main.c and started task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function to update the value of a pointer to 98
+ */
+
+void reset_t0_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a  folder containing tasks on pointers, arrays and strings in C
